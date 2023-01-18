70b121b26a20035d691da818b8c878394cb8c9f8
SImon Lucas <simonmarklucas@gmail.com>
2023-01-18 19:19:07 +0000

    Update run_single_game.py
    Fixed bugs game runner, now generates a run of a game e.g.:
    
    Player PlayerId.CHANCE took action 1 in state: k=-1, a={0, 1, 2, 3}, s=set(), m=0
    Player 0 took action 3 in state: k=1, a={0, 1, 2, 3}, s=set(), m=1
    Player 1 took action 2 in state: k=1, a={0, 1, 2}, s=set(), m=2
    Player 0 took action 0 in state: k=1, a={0, 1, 2}, s={2}, m=3
    Final state: k=1, a={1, 2}, s={2}, m=4
    [-30, 30]

--- SimpleMurderMystery/murderspiel/run_single_game.py
+++ SimpleMurderMystery/murderspiel/run_single_game.py
@@ -1,7 +1,9 @@
 from typing import List
 import numpy as np
+import numpy.typing as npt
 
 from open_spiel.python.algorithms import exploitability, cfr
+from pyspiel import TabularPolicy
 
 from murderspiel.pyspiel_murder_variations import MurderMysteryVariationsGame, MurderMysteryParams
 import itertools as it
@@ -9,12 +11,12 @@ from open_spiel.python import policy as policy_lib
 
 
 # now play some games against each other
-def sample(actions_and_probs):
+def sample(actions_and_probs: list):
     actions, probs = zip(*actions_and_probs)
     return np.random.choice(actions, p=probs)
 
 
-def policy_as_list(policy, state):
+def policy_as_list(policy: TabularPolicy, state: MurderMysteryVariationsGame):
     # print(policy)
     # print(state)
 
@@ -24,7 +26,7 @@ def policy_as_list(policy, state):
 
 
 # this should work for any openspiel game, but I've been lazy and put this type for now
-def run_game(game: MurderMysteryVariationsGame, players: List[policy]) -> List[float]:
+def run_game(game: MurderMysteryVariationsGame, players: List[TabularPolicy]) -> List[float]:
     state = game.new_initial_state()
     while not state.is_terminal():
         if state.is_chance_node():
@@ -33,13 +35,17 @@ def run_game(game: MurderMysteryVariationsGame, players: List[policy]) -> List[f
             player_policy = players[state.current_player()]
             action_probs = policy_as_list(player_policy, state)
         action = sample(action_probs)
+        print(f"Player {state.current_player()} took action {action} in state: {state}")
         state = state.child(action)
+    print(f"Final state: {state}")
     return state.returns()
 
 
 if __name__ == '__main__':
-    game = MurderMysteryVariationsGame()
+    params = MurderMysteryParams(allow_pass=True, allow_suicide=False)
+    game = MurderMysteryVariationsGame(game_params=params)
     # create a uniform random policy
+    print(game.game_params)
     uniform_random_policy = policy_lib.TabularPolicy(game)
     player_policies = [uniform_random_policy, uniform_random_policy]
     result = run_game(game, player_policies)
